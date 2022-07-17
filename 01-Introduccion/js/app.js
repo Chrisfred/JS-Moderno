9d3f7f1a5a83344c38ab9c4fed161a9d2c36cf7a
Chris Cuenca <chrisfred86@gmail.com>
2022-07-16 21:02:10 -0500

    Agrego archivo eslint para calidad de código

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,2 +1,8 @@
-alert("Hola mundo");
-alert ("Hola mundo con alert 2")
+const nombre = prompt ("Cuál es tu nombre?");
+// Toma le nombre y muestra en la pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} Esta aprendiento JS`;
+
+/* RECOMENDACIONES
+1 Instrucción por linea
+2 tab para ordenar el código
+*/
